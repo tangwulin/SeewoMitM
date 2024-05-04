c0f6285002f96cc32a349fc332d9a6be4ebf5eea
tangwulin <3498665850@qq.com>
2024-05-04 22:12:52 +0800

    feat:用来下载资源的下载器

---
+++ internal/downloader/downloader.go
@@ -0,0 +1,50 @@
+package downloader
+
+import (
+	"SeewoMitM/internal/log"
+	"io"
+	"net/http"
+	"os"
+)
+
+type downloadTask struct {
+	Url  string
+	Path string
+}
+
+var downloadQueue = make(chan downloadTask, 1000)
+
+func LaunchDownloader(threads int) {
+	for i := 0; i < threads; i++ {
+		go func() {
+			log.WithFields(log.Fields{"type": "Downloader"}).Infof("downloader %d# started", i)
+			for {
+				task := <-downloadQueue
+				file, err := os.OpenFile(task.Path, os.O_CREATE|os.O_WRONLY, os.ModePerm)
+				if err != nil {
+					log.WithFields(log.Fields{"type": "Downloader"}).Error("failed to open file:", task.Path)
+					continue
+				}
+				defer file.Close()
+
+				resp, err := http.Get(task.Url)
+				if err != nil {
+					log.WithFields(log.Fields{"type": "Downloader"}).Error("failed to download file:", task.Url)
+					continue
+				}
+				defer resp.Body.Close()
+
+				_, err = io.Copy(file, resp.Body)
+				if err != nil {
+					log.WithFields(log.Fields{"type": "Downloader"}).Error("failed to write file:", task.Path)
+					continue
+				}
+				log.WithFields(log.Fields{"type": "Downloader"}).Info("downloaded file:", task.Path)
+			}
+		}()
+	}
+}
+
+func AddDownloadTask(url, path string) {
+	downloadQueue <- downloadTask{Url: url, Path: path}
+}
