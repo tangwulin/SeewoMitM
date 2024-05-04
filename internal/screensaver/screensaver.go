c16a3ea7523d404f961b7021777469b43dd4bac7
tangwulin <3498665850@qq.com>
2024-05-04 23:12:53 +0800

    feat:屏保相关

---
+++ internal/screensaver/screensaver.go
@@ -0,0 +1,102 @@
+package screensaver
+
+import (
+	"SeewoMitM/internal/config"
+	"SeewoMitM/internal/helper"
+	"SeewoMitM/internal/log"
+	"SeewoMitM/internal/resource"
+	"strings"
+)
+
+var screensaverDataContents DataContent
+
+type DataContent struct {
+	ImageList    []string     `json:"imageList"`
+	ExtraPayload ExtraPayload `json:"extraPayload,omitempty"`
+}
+
+type ExtraPayload struct {
+	ScreensaverContent []Content `json:"screensaverContent"`
+}
+
+func ParseScreensaverContent() DataContent {
+	gc := helper.GetConfig()
+	if gc.ScreensaverConfig == nil {
+		return DataContent{}
+	}
+
+	imgList := make([]string, 0)
+	contents := make([]Content, 0)
+
+	for _, c := range gc.ScreensaverConfig.Contents {
+		switch c.Type {
+		case "image":
+			url := GetContentTrueUrl(c)
+
+			imgList = append(imgList, url)
+			contents = append(contents, *NewImageContent(url, c.Fit, c.Duration))
+		case "video":
+			contents = append(contents, *NewVideoContent(GetContentTrueUrl(c), c.Fit, c.Duration))
+		case "spine":
+
+			// 如果有RawDataURIs就直接使用
+			if len(c.SpinePlayerConfig.RawDataURIs) > 0 {
+				contents = append(contents, *NewSpineContent(c.Path, c.SpinePlayerConfig, c.Duration))
+				continue
+			}
+
+			// 如果没有RawDataURIs就使用AtlasUrl和JsonUrl
+			spinePlayerConfig := *c.SpinePlayerConfig
+			var atlasUrl string
+			if spinePlayerConfig.AtlasUrl != "" {
+				atlasUrl = GetResourceTrueUrl(spinePlayerConfig.AtlasUrl, c.RequirePreload)
+			}
+			var jsonUrl string
+			if spinePlayerConfig.JsonUrl != "" {
+				jsonUrl = GetResourceTrueUrl(spinePlayerConfig.JsonUrl, c.RequirePreload)
+			}
+			var backgroundImageUrl string
+			if spinePlayerConfig.BackgroundImage.Url != "" {
+				backgroundImageUrl = GetResourceTrueUrl(spinePlayerConfig.BackgroundImage.Url, c.RequirePreload)
+			}
+
+			// 写回去
+			spinePlayerConfig.AtlasUrl = atlasUrl
+			spinePlayerConfig.JsonUrl = jsonUrl
+			spinePlayerConfig.BackgroundImage.Url = backgroundImageUrl
+			contents = append(contents, *NewSpineContent(c.Path, &spinePlayerConfig, c.Duration))
+		default:
+			log.WithFields(log.Fields{"type": "ParseScreensaverContent"}).Error("unknown content type, content will be ignored:", c)
+			continue
+		}
+	}
+
+	return DataContent{
+		ImageList:    imgList,
+		ExtraPayload: ExtraPayload{ScreensaverContent: contents},
+	}
+}
+
+func GetContentTrueUrl(c config.ScreensaverContent) string {
+	if c.IsRequirePreload() {
+		md5 := helper.MD5Sum([]byte(c.Path))
+		resource.PrepareResource(c.Path, md5)
+		return resource.GetResourceServerAddr() + "/" + md5
+	}
+	return c.Path
+}
+
+func GetResourceTrueUrl(url string, requirePreload bool) string {
+	if strings.HasPrefix(url, "http") && requirePreload {
+		return resource.GetResourceServerAddr() + "/" + url
+	}
+	return url
+}
+
+func LoadScreensaverData() {
+	screensaverDataContents = ParseScreensaverContent()
+}
+
+func GetScreensaverData() *DataContent {
+	return &screensaverDataContents
+}
