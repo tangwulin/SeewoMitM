ba671b6f2e04aca3c40effc5ac0be571d0dba5ca
tangwulin <3498665850@qq.com>
2024-05-21 18:40:33 +0800

    feat:去除暂时不需要的函数

--- screensaver.go
+++ screensaver.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"SeewoMitM/internal/helper"
 	"SeewoMitM/internal/log"
 	"SeewoMitM/model"
 	"github.com/spf13/viper"
@@ -19,34 +18,13 @@ func ParseScreensaverContent() model.DataContent {
 	for _, c := range screensaverConfig.Contents {
 		switch c.Type {
 		case "image":
-			url := GetContentTrueUrl(c)
+			url := c.Path
 
 			imgList = append(imgList, url)
 			contents = append(contents, *NewImageContent(url, c.Fit, c.Duration))
 		case "video":
-			contents = append(contents, *NewVideoContent(GetContentTrueUrl(c), c.Fit, c.Duration))
+			contents = append(contents, *NewVideoContent(c.Path, c.Fit, c.Duration))
 		case "spine":
-			/*// 如果有RawDataURIs就直接使用
-			if len(c.SpinePlayerConfig.RawDataURIs) > 0 {
-				contents = append(contents, *NewSpineContent(c.Path, c.SpinePlayerConfig, c.Duration))
-				continue
-			}
-
-			// 如果没有RawDataURIs就使用AtlasUrl和JsonUrl
-			spinePlayerConfig := *c.SpinePlayerConfig
-
-			if &spinePlayerConfig.AtlasUrl != nil && spinePlayerConfig.AtlasUrl != "" {
-				spinePlayerConfig.AtlasUrl = GetResourceTrueUrl(spinePlayerConfig.AtlasUrl, c.RequirePreload)
-			}
-
-			if &spinePlayerConfig.JsonUrl != nil && spinePlayerConfig.JsonUrl != "" {
-				spinePlayerConfig.JsonUrl = GetResourceTrueUrl(spinePlayerConfig.JsonUrl, c.RequirePreload)
-			}
-
-			if spinePlayerConfig.BackgroundImage != nil && spinePlayerConfig.BackgroundImage.Url != "" {
-				spinePlayerConfig.BackgroundImage.Url = GetResourceTrueUrl(spinePlayerConfig.BackgroundImage.Url, c.RequirePreload)
-			}*/
-
 			contents = append(contents, *NewSpineContent(c.Path, c.SpinePlayerConfig, c.Duration, c.Scale, c.OffsetX, c.OffsetY))
 		default:
 			log.WithFields(log.Fields{"type": "ParseScreensaverContent"}).Error("unknown content type, content will be ignored:", c)
@@ -73,22 +51,6 @@ func ParseScreensaverContent() model.DataContent {
 		SwitchInterval: screensaverConfig.SwitchInterval / 1000,
 		TextList:       textList,
 	}
-}
-
-func GetContentTrueUrl(c model.ScreensaverContent) string {
-	if c.IsRequirePreload() {
-		md5 := helper.MD5Sum([]byte(c.Path))
-		PrepareResource(c.Path, md5)
-		return GetResourceServerAddr() + "/" + md5
-	}
-	return c.Path
-}
-
-func GetResourceTrueUrl(url string, requirePreload bool) string {
-	if strings.HasPrefix(url, "http") && requirePreload {
-		return GetResourceServerAddr() + "/" + url
-	}
-	return url
 }
 
 func LoadScreensaverContent() {
